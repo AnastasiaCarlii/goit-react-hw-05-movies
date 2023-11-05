93d57b2ed992bb1f02c9360752bd91f379ca08d1
Anastasia Carlii <nastjaorosszul@gmail.com>
2023-11-05 21:25:28 +0000

    pages created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
+import Layout from './Layout/Layout';
+import Movies from 'pages/Movies';
+import Home from 'pages/Home';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/movies">Movies</NavLink>
+        </li>
+      </ul>
+
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+        </Route>
+      </Routes>
     </div>
   );
 };
